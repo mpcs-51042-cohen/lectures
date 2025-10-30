0e9e2dd845048027ebb40f27fbd73d7de6d213d2
Jeff Cohen <cohen.jeff@gmail.com>
2025-10-30 15:05:23 -0500

    Ready for Week 5

---
+++ week5/README.md
@@ -0,0 +1,30 @@
+# Week 5
+
+### Agenda
+
+0. Test fixtures, running project-specific utilities with `uv run`
+
+2. Lab #5: Practice with classes and objects
+
+--- BREAK ---
+
+3. Classes and Objects: 
+  - Encapsulation
+  - Special Methods
+  - Duck Typing
+
+4. Retrieving web-based data with the Requests package
+  - We're using themoviedb.org
+  - Challenge #1: Output a list movie titles
+  - Challenge #2: Output a nicely-formatted display of a single movie
+
+5. Lessons from the Apollo space program
+  - Work incrementally
+  - Think in terms of modular encapsulation
+  - "Systems thinking"
+  - Quality control
+
+6. Rubber duck debugging
+
+
+
---
+++ week5/movies.py
@@ -0,0 +1,45 @@
+import requests
+import base64
+
+# Here are some helpful functions.
+
+def getApiKey(): 
+  return base64.b64.decode('YmRlMDI0ZjNlYjQzZjU5N2FhZmUwMWVkOWM5MDk4YzY=').decode()
+
+# Resource can be "top_rated" or "now_playing"
+def url_for_movies(resource):
+  apiKey = f"api_key={getApiKey()}"
+  language = "language=en-US"
+  region = "region=US"
+  filter = "include_adult=false"
+  base_url = f"https://api.themoviedb.org/3/movie/{resource}"
+  return f"{base_url}?{apiKey}&{language}&{region}&{filter}"
+
+
+# Pass in the movie title or keyword to search for
+def url_for_search(keyword):
+  apiKey = f"api_key=${getApiKey()}"
+  language = "language=en-US"
+  search = f"query={keyword}"
+  region = "region=US"
+  filter = "include_adult=false"
+  base_url = "https://api.themoviedb.org/3/search/movie"
+  return f"{base_url}?{apiKey}&{search}&{language}&{region}&{filter}"
+
+
+# Example Usage
+
+url = url_for_movies('top_rated')
+print("Calling", url)
+
+response = requests.get(url)
+
+if response.status_code == 200:
+  results = response.json()
+  print("Got results!")
+  # TO DO:
+  # Display movie titles and rating + whatever else seems interesting
+  
+else:
+  print("Request failed", response.status_code)
+
